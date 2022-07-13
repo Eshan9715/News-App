8bd6420f8f6ea2a3a5c59606328cc400f87e9dba
Eshan Amanda <eshanesn97@gmail.com>
2022-07-13 06:08:15 +0530

    Add recommeneded changes

--- app/src/main/java/com/example/newsapp/Models/NewsApiResponse.java
+++ app/src/main/java/com/example/newsapp/Models/NewsApiResponse.java
@@ -1,8 +1,9 @@
 package com.example.newsapp.Models;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class NewsApiResponse {
+public class NewsApiResponse implements Serializable {
     String status;
     int totalResults;
     List<NewsHeadlines> articles;
--- app/src/main/java/com/example/newsapp/Models/NewsHeadlines.java
+++ app/src/main/java/com/example/newsapp/Models/NewsHeadlines.java
@@ -1,6 +1,8 @@
 package com.example.newsapp.Models;
 
-public class NewsHeadlines {
+import java.io.Serializable;
+
+public class NewsHeadlines implements Serializable {
     Source source = null;
     String author = "";
     String title = "";
